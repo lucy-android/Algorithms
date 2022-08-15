f9e2f6584c79f780bc5e64128330703be5decab2
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-08-15 18:26:29 +0300

    the correctness of the new solution is confirmed by the checking system

---
+++ src/priority/queue/decode/huffman/code/Main.java
@@ -0,0 +1,46 @@
+package priority.queue.decode.huffman.code;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String readString = readFile();
+        writeIntoFile(("C:\\Users\\User\\IdeaProjects\\Algorithms\\src\\priority\\" +
+                        "queue\\decode\\huffman\\code\\output.txt"),
+                "You greet me " + readString + " Hello first file");
+    }
+
+    private static void writeIntoFile(String path, String writtenString) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(path)) {
+            byte[] bytes = writtenString.getBytes();
+            for(byte bt: bytes){
+                fileOutputStream.write(bt);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String readFile() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (FileInputStream fis = new FileInputStream("C:\\Users\\User\\IdeaProjects\\Algorithms" +
+                "\\src\\priority\\queue\\decode\\huffman\\code\\input.txt")) {
+            int content;
+            // reads a byte at a time, if it reached end of the file, returns -1
+            while ((content = fis.read()) != -1) {
+                char castContent = (char) content;
+                System.out.print(castContent);
+                stringBuilder.append(castContent);
+            }
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
