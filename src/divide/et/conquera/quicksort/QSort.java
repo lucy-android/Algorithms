9fce4c057f2d1dfeb80d52496352fd4d24501e94
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-08-20 20:48:13 +0300

    tested the code of selectPivotIndex method

--- src/divide/et/conquera/quicksort/QSort.java
+++ src/divide/et/conquera/quicksort/QSort.java
@@ -4,11 +4,14 @@ import java.util.Arrays;
 
 class QSort {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        int[] intArray = new int[]{7, 2, 1, 8, 6, 3, 5, 3422, 685, 100, 1, 11, 0 ,0 ,0};
+        int[] intArray = new int[]{7, 2, 1, 8, 6, 3, 5, 3422, 685, 100, 1, 11, 0, 0, 0};
 
-        System.out.println(Arrays.toString(partitionArray(intArray, 0, 14)));
+        // System.out.println(Arrays.toString(partitionArray(intArray, 0, 14)));
+
+        System.out.println(selectPivotIndex(
+                new int[]{7, 2, 1, 8}, 0, 3));
         /*int pivotIndex = selectPivotIndex(intArray);
         System.out.println(selectPivotIndex(intArray));
         int pivot = intArray[pivotIndex];
@@ -72,8 +75,19 @@ class QSort {
 
     }
 
-    private static int selectPivotIndex(int[] initialArray, int startIndex, int endIndex) {
-        int sumOfElements = 0;
+    private static int selectPivotIndex(int[] initialArray, int startIndex, int endIndex) throws Exception {
+        if (endIndex > initialArray.length || startIndex > initialArray.length) {
+            throw new Exception("Error in the method selectPivotIndex: endIndex > initialArray.length || startIndex > initialArray.length!");
+        } else if (endIndex < startIndex) {
+            throw new Exception("Error in the method selectPivotIndex: endIndex < startIndex!");
+        } else if (startIndex < 0) {
+            throw new Exception("Error in the method selectPivotIndex: startIndex<0!");
+        } else if (endIndex == startIndex) {
+            return initialArray[endIndex];
+        } else if (initialArray.length == 1) {
+            return 0;
+        }
+            int sumOfElements = 0;
         for (int j = startIndex; j <= endIndex; j++) {
             sumOfElements += initialArray[j];
         }
@@ -83,7 +97,7 @@ class QSort {
         int closestToAverage = -1;
 
         for (int i = startIndex; i <= endIndex; i++) {
-            if (Math.abs(average - closestToAverage) > Math.abs(average - initialArray[i])) {
+            if (Math.abs(average - closestToAverage) >= Math.abs(average - initialArray[i])) {
                 closestToAverage = initialArray[i];
                 position = i;
             }
@@ -92,7 +106,7 @@ class QSort {
         return position;
     }
 
-    private static int[] partitionArray(int[] arrayToBePartitioned, int startIndex, int endIndex) {
+    private static int[] partitionArray(int[] arrayToBePartitioned, int startIndex, int endIndex) throws Exception {
         int i = startIndex - 1;
         int j = startIndex;
         int pivotIndex = selectPivotIndex(arrayToBePartitioned, startIndex, endIndex);
