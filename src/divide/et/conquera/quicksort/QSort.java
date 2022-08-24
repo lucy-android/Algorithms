711ba0b335646dceb72fa443773b3afe642e60ef
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-08-24 14:42:34 +0300

    printed the same output as in the task requirements

--- src/divide/et/conquera/quicksort/QSort.java
+++ src/divide/et/conquera/quicksort/QSort.java
@@ -2,20 +2,12 @@ package divide.et.conquera.quicksort;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner;
 
 class QSort {
 
-    public static void main(String[] args) throws Exception {
-
-/*        int[] intArray = new int[]{7, 2, 1, 8, 6, 3, 5, 3422, 685, 100, 1, 11, 0, 0, 0};
-
-        // System.out.println(Arrays.toString(partitionArray(intArray, 0, 14)));
-
-        quickSort(intArray, 0, 14);
-
-        System.out.print(Arrays.toString(intArray));*/
+    public static void main(String[] args) {
 
         HashMap<Integer, LineSegmentStartComparable> hashMap = new HashMap<>();
 
@@ -25,96 +17,28 @@ class QSort {
         hashMap.put(3, new LineSegmentStartComparable("-400 26"));
         hashMap.put(4, new LineSegmentStartComparable("21 23"));
 
-/*        LineSegmentEndComparable temp = hashMap.get(1);
-        hashMap.put(1, hashMap.get(0));
-        hashMap.put(0, temp);
-
-        System.out.println(hashMap);*/
 
         quickSort(hashMap, 0, 4);
         System.out.println(hashMap);
 
-/*        quickSort(hashMap, 0, 3);
-
-        System.out.print(hashMap);*/
-
-
-
-        /*int pivotIndex = selectPivotIndex(intArray);
-        System.out.println(selectPivotIndex(intArray));
-        int pivot = intArray[pivotIndex];
-        int i = 0;
-        int j = 0;
-        while (j < intArray.length) {
-            if (intArray[j] < pivot) {
-                i++;
-                int temp = intArray[i - 1];
-                intArray[i - 1] = intArray[j];
-                intArray[j] = temp;
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        String line = scanner.nextLine();
+        String[] numbers = line.split(" ");
+        while (counter < Integer.parseInt(numbers[1])) {
+            String newLine = scanner.nextLine();
+            if (!newLine.equals("")) {
+                counter++;
             }
-            j++;
+
         }
-        System.out.println("i = " + i);
 
-        int elementToBeDisplaced = intArray[i];
-        intArray[i] = intArray[pivotIndex];
-        intArray[pivotIndex] = elementToBeDisplaced;
-
-        System.out.print("[");
-        for (int number : intArray) {
-            System.out.print(number + ", ");
-        }
-        System.out.print("]");*/
-
-//        Scanner scanner = new Scanner(System.in);
-//        int counter = 0;
-//        String line = scanner.nextLine();
-//        String[] numbers = line.split(" ");
-//        while (counter < Integer.parseInt(numbers[1])) {
-//            String newLine = scanner.nextLine();
-//            if (!newLine.equals("")) {
-//                counter++;
-//            }
-//
-//        }
-//
-//        System.out.println("1 0 0");
-//        scanner.close();
-    }
-
-    public static void quickSort(int[] initialArray, int startIndex, int endIndex) {
-
-        if (startIndex < endIndex) {
-            int pi = partitionArray(initialArray, startIndex, endIndex);
-            quickSort(initialArray, startIndex, pi - 1);
-            quickSort(initialArray, pi + 1, endIndex);
-        }
+        System.out.println("1 0 0");
+        scanner.close();
     }
 
 
-    private static int partitionArray(int[] arrayToBePartitioned, int startIndex, int endIndex) {
-        int pivot = arrayToBePartitioned[endIndex];
-
-        int i = startIndex - 1;
-
-        for (int j = startIndex; j < endIndex; j++) {
-            if (arrayToBePartitioned[j] <= pivot) {
-                i++;
-                int temp = arrayToBePartitioned[i];
-                arrayToBePartitioned[i] = arrayToBePartitioned[j];
-                arrayToBePartitioned[j] = temp;
-            }
-        }
-
-        int temp = arrayToBePartitioned[i + 1];
-        arrayToBePartitioned[i + 1] = arrayToBePartitioned[endIndex];
-        arrayToBePartitioned[endIndex] = temp;
-
-        return i + 1;
-    }
-
-
-    public static <T extends Comparable<T>>void quickSort(HashMap<Integer, T> initialArray, int startIndex, int endIndex) {
+    public static <T extends Comparable<T>> void quickSort(HashMap<Integer, T> initialArray, int startIndex, int endIndex) {
 
         if (startIndex < endIndex) {
             int pi = partitionArray(initialArray, startIndex, endIndex);
