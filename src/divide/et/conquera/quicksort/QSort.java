f292ff8047063f0218b85efd50f3f016eaa760e0
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-08-28 13:42:02 +0300

    printed out the result according to the task specifications

--- src/divide/et/conquera/quicksort/QSort.java
+++ src/divide/et/conquera/quicksort/QSort.java
@@ -1,6 +1,5 @@
 package divide.et.conquera.quicksort;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 class QSort {
@@ -40,9 +39,8 @@ class QSort {
                     int m = countAllSmallerElementsInAnArray(lineSegmentEndPoints, intPointArray[k]);
                     resultArray[k] = n - m;
                 }
-                System.out.println(Arrays.toString(intPointArray));
                 String[] stringResultArray = new String[resultArray.length];
-                for(int x = 0; x< resultArray.length; x++){
+                for (int x = 0; x < resultArray.length; x++) {
                     stringResultArray[x] = String.valueOf(resultArray[x]);
                 }
                 System.out.println(String.join(" ", stringResultArray));
@@ -51,11 +49,6 @@ class QSort {
         int length = lineSegmentStartPoints.length - 1;
         quickSort(lineSegmentStartPoints, 0, length);
         quickSort(lineSegmentEndPoints, 0, length);
-        System.out.println(Arrays.toString(lineSegmentStartPoints));
-        System.out.println(Arrays.toString(lineSegmentEndPoints));
-
-
-        System.out.println("1 0 0");
 
         scanner.close();
     }
@@ -117,109 +110,3 @@ class QSort {
 
 
 }
-
-
-/**
- * import java.util.Arrays;
- * import java.util.Scanner;
- * <p>
- * class QSort {
- * <p>
- * public static void main(String[] args) {
- * <p>
- * Scanner scanner = new Scanner(System.in);
- * int counter = 0;
- * String line = scanner.nextLine();
- * String[] numbers = line.split(" ");
- * int lineSegmentNumber = Integer.parseInt(numbers[0]);
- * int[] lineSegmentStartPoints = new int[lineSegmentNumber];
- * int[] lineSegmentEndPoints = new int[lineSegmentNumber];
- * while (counter <= 1 + lineSegmentNumber) {
- * String newLine = scanner.nextLine();
- * if (!newLine.equals("")) {
- * if (counter < lineSegmentNumber) {
- * String[] lineSegment = newLine.split(" ");
- * int firstNumber = Integer.parseInt(lineSegment[0]);
- * int secondNumber = Integer.parseInt(lineSegment[1]);
- * lineSegmentStartPoints[counter] = firstNumber;
- * lineSegmentEndPoints[counter] = secondNumber;
- * }
- * counter++;
- * }
- * String[] lastStringArray = newLine.split(" ");
- * int[] pointArray = new int[lastStringArray.length];
- * if (counter == lineSegmentNumber + 1) {
- * for (int i = 0; i < lastStringArray.length; i++) {
- * pointArray[i] = Integer.parseInt(lastStringArray[i]);
- * }
- * }
- * //TODO subject each point in pointArray to the first and the second array and add the results to the third array and print in out
- * int[] resultArray = new int[pointArray.length];
- * for (int k = 0; k < pointArray.length; k++){
- * int point = pointArray[k];
- * int n = countAllNotGreaterElementsInAnArray(pointArray, point);
- * int m = countAllGreaterElementsInAnArray(pointArray, point);
- * int result = n - m;
- * resultArray[k] = result;
- * }
- * System.out.println(Arrays.toString(resultArray));
- * <p>
- * }
- * <p>
- * int length = lineSegmentStartPoints.length - 1;
- * quickSort(lineSegmentStartPoints, 0, length);
- * quickSort(lineSegmentEndPoints, 0, length);
- * System.out.println(Arrays.toString(lineSegmentStartPoints));
- * System.out.println(Arrays.toString(lineSegmentEndPoints));
- * <p>
- * System.out.println("1 0 0");
- * <p>
- * scanner.close();
- * }
- * <p>
- * public static void quickSort(int[] initialArray, int startIndex, int endIndex) {
- * <p>
- * if (startIndex < endIndex) {
- * int pi = partitionArray(initialArray, startIndex, endIndex);
- * quickSort(initialArray, startIndex, pi - 1);
- * quickSort(initialArray, pi + 1, endIndex);
- * }
- * }
- * <p>
- * public static int countAllNotGreaterElementsInAnArray(int[] initialArray, int point) {
- * int counter = 0;
- * while (point > initialArray[counter]) {
- * counter++;
- * }
- * return counter;
- * }
- * <p>
- * public static int countAllGreaterElementsInAnArray(int[] initialArray, int point) {
- * return initialArray.length - countAllNotGreaterElementsInAnArray(initialArray, point);
- * }
- * <p>
- * <p>
- * private static int partitionArray(int[] arrayToBePartitioned, int startIndex, int endIndex) {
- * int pivot = arrayToBePartitioned[endIndex];
- * <p>
- * int i = startIndex - 1;
- * <p>
- * for (int j = startIndex; j < endIndex; j++) {
- * if (arrayToBePartitioned[j] <= pivot) {
- * i++;
- * int temp = arrayToBePartitioned[i];
- * arrayToBePartitioned[i] = arrayToBePartitioned[j];
- * arrayToBePartitioned[j] = temp;
- * }
- * }
- * <p>
- * int temp = arrayToBePartitioned[i + 1];
- * arrayToBePartitioned[i + 1] = arrayToBePartitioned[endIndex];
- * arrayToBePartitioned[endIndex] = temp;
- * <p>
- * return i + 1;
- * }
- * <p>
- * <p>
- * }
- */
