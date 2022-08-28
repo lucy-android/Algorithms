680cf5eb0a7501fffc9c29d30bf333ad5d4228a6
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-08-28 12:49:55 +0300

    rolled back to restore the previous result

--- src/divide/et/conquera/quicksort/QSort.java
+++ src/divide/et/conquera/quicksort/QSort.java
@@ -1,12 +1,15 @@
 package divide.et.conquera.quicksort;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 class QSort {
 
     public static void main(String[] args) {
 
-        System.out.println(countAllGreaterElementsInAnArray(new int[]{1, 3, 7, 11, 14, 16, 34}, 12));
+        // System.out.println(countAllGreaterElementsInAnArray(new int[]{1, 3, 7, 11, 14, 16, 34}, 12));
 
-        /*Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int counter = 0;
         String line = scanner.nextLine();
         String[] numbers = line.split(" ");
@@ -36,7 +39,7 @@ class QSort {
 
         System.out.println("1 0 0");
         //TODO
-        scanner.close();*/
+        scanner.close();
     }
 
     public static void quickSort(int[] initialArray, int startIndex, int endIndex) {
@@ -87,131 +90,106 @@ class QSort {
 
 
 /**
- * class QSort {
- * <p>
- * public static void main(String[] args) {
- * <p>
- * HashMap<Integer, LineSegmentStartComparable> hashMap = new HashMap<>();
- * <p>
- * hashMap.put(0, new LineSegmentStartComparable("70 78"));
- * hashMap.put(1, new LineSegmentStartComparable("5 6"));
- * hashMap.put(2, new LineSegmentStartComparable("9 10"));
- * hashMap.put(3, new LineSegmentStartComparable("-400 26"));
- * hashMap.put(4, new LineSegmentStartComparable("21 23"));
- * <p>
- * <p>
- * quickSort(hashMap, 0, 4);
- * System.out.println(hashMap);
- * <p>
- * Scanner scanner = new Scanner(System.in);
- * int counter = 0;
- * String line = scanner.nextLine();
- * String[] numbers = line.split(" ");
- * HashMap<Integer, LineSegmentStartComparable> lineSegmentHashMap = new HashMap<>();
- * while (counter < 1 + Integer.parseInt(numbers[0])) {
- * String newLine = scanner.nextLine();
- * LineSegmentStartComparable lineSegment = new LineSegmentStartComparable(newLine);
- * if (!newLine.equals("")) {
- * if(counter < Integer.parseInt(numbers[0])){
- * lineSegmentHashMap.put(counter, lineSegment);
- * }
- * counter++;
- * }
- * <p>
- * }
- * <p>
- * System.out.println("1 0 0");
- * System.out.println(lineSegmentHashMap);
- * scanner.close();
- * }
- * <p>
- * <p>
- * public static <T extends Comparable<T>> void quickSort(HashMap<Integer, T> initialArray, int startIndex, int endIndex) {
- * <p>
- * if (startIndex < endIndex) {
- * int pi = partitionArray(initialArray, startIndex, endIndex);
- * quickSort(initialArray, startIndex, pi - 1);
- * quickSort(initialArray, pi + 1, endIndex);
- * }
- * }
- * <p>
- * <p>
- * private static <T extends Comparable<T>> int partitionArray(HashMap<Integer, T> arrayToBePartitioned, int startIndex, int endIndex) {
- * T pivot = arrayToBePartitioned.get(endIndex);
- * <p>
- * int i = startIndex - 1;
- * <p>
- * for (int j = startIndex; j < endIndex; j++) {
- * T end = arrayToBePartitioned.get(j);
- * if (end.compareTo(pivot) <= 0) {
- * i++;
- * T temp = arrayToBePartitioned.get(i);
- * arrayToBePartitioned.put(i, arrayToBePartitioned.get(j));
- * arrayToBePartitioned.put(j, temp);
- * }
- * }
- * <p>
- * //swap arrayToBePartitioned[i+1] and arrayToBePartitioned[endIndex]
- * <p>
- * T temp = arrayToBePartitioned.get(i + 1);
- * arrayToBePartitioned.put(i + 1, arrayToBePartitioned.get(endIndex));
- * arrayToBePartitioned.put(endIndex, temp);
- * <p>
- * return i + 1;
- * }
- * <p>
- * <p>
- * }
- * <p>
- * class LineSegmentBase {
- * <p>
- * final int number1;
- * final int number2;
- * <p>
- * public LineSegmentBase(String twoNumbers) {
- * String[] numberArray = twoNumbers.split(" ");
- * this.number1 = Integer.parseInt(numberArray[0]);
- * this.number2 = Integer.parseInt(numberArray[1]);
- * }
- * <p>
- * }
- * <p>
- * class LineSegmentStartComparable extends LineSegmentBase implements Comparable<LineSegmentStartComparable> {
- * <p>
- * public LineSegmentStartComparable(String twoNumbers) {
- * super(twoNumbers);
- * }
+ * import java.util.Arrays;
+ * import java.util.Scanner;
  *
- * @Override public int compareTo(@NotNull LineSegmentStartComparable o) {
- * if (this.number1 > o.number1) {
- * return 1;
- * } else if (this.number1 < o.number1) {
- * return -1;
- * } else {
- * return 0;
- * }
- * }
- * @Override public String toString() {
- * return number1 + " " + number2;
- * }
- * }
- * <p>
- * class LineSegmentEndComparable extends LineSegmentBase implements Comparable<LineSegmentEndComparable> {
- * <p>
- * public LineSegmentEndComparable(String twoNumbers) {
- * super(twoNumbers);
- * }
- * @Override public int compareTo(@NotNull LineSegmentEndComparable o) {
- * if (this.number2 > o.number2) {
- * return 1;
- * } else if (this.number2 < o.number2) {
- * return -1;
- * } else {
- * return 0;
- * }
- * }
- * @Override public String toString() {
- * return number1 + " " + number2;
- * }
+ * class QSort {
+ *
+ *     public static void main(String[] args) {
+ *
+ *         Scanner scanner = new Scanner(System.in);
+ *         int counter = 0;
+ *         String line = scanner.nextLine();
+ *         String[] numbers = line.split(" ");
+ *         int lineSegmentNumber = Integer.parseInt(numbers[0]);
+ *         int[] lineSegmentStartPoints = new int[lineSegmentNumber];
+ *         int[] lineSegmentEndPoints = new int[lineSegmentNumber];
+ *         while (counter <= 1 + lineSegmentNumber) {
+ *             String newLine = scanner.nextLine();
+ *             if (!newLine.equals("")) {
+ *                 if (counter < lineSegmentNumber) {
+ *                     String[] lineSegment = newLine.split(" ");
+ *                     int firstNumber = Integer.parseInt(lineSegment[0]);
+ *                     int secondNumber = Integer.parseInt(lineSegment[1]);
+ *                     lineSegmentStartPoints[counter] = firstNumber;
+ *                     lineSegmentEndPoints[counter] = secondNumber;
+ *                 }
+ *                 counter++;
+ *             }
+ *             String[] lastStringArray = newLine.split(" ");
+ *             int[] pointArray = new int[lastStringArray.length];
+ *             if (counter == lineSegmentNumber + 1) {
+ *                 for (int i = 0; i < lastStringArray.length; i++) {
+ *                     pointArray[i] = Integer.parseInt(lastStringArray[i]);
+ *                 }
+ *             }
+ *             //TODO subject each point in pointArray to the first and the second array and add the results to the third array and print in out
+ *             int[] resultArray = new int[pointArray.length];
+ *             for (int k = 0; k < pointArray.length; k++){
+ *                 int point = pointArray[k];
+ *                 int n = countAllNotGreaterElementsInAnArray(pointArray, point);
+ *                 int m = countAllGreaterElementsInAnArray(pointArray, point);
+ *                 int result = n - m;
+ *                 resultArray[k] = result;
+ *             }
+ *             System.out.println(Arrays.toString(resultArray));
+ *
+ *         }
+ *
+ *         int length = lineSegmentStartPoints.length - 1;
+ *         quickSort(lineSegmentStartPoints, 0, length);
+ *         quickSort(lineSegmentEndPoints, 0, length);
+ *         System.out.println(Arrays.toString(lineSegmentStartPoints));
+ *         System.out.println(Arrays.toString(lineSegmentEndPoints));
+ *
+ *         System.out.println("1 0 0");
+ *
+ *         scanner.close();
+ *     }
+ *
+ *     public static void quickSort(int[] initialArray, int startIndex, int endIndex) {
+ *
+ *         if (startIndex < endIndex) {
+ *             int pi = partitionArray(initialArray, startIndex, endIndex);
+ *             quickSort(initialArray, startIndex, pi - 1);
+ *             quickSort(initialArray, pi + 1, endIndex);
+ *         }
+ *     }
+ *
+ *     public static int countAllNotGreaterElementsInAnArray(int[] initialArray, int point) {
+ *         int counter = 0;
+ *         while (point > initialArray[counter]) {
+ *             counter++;
+ *         }
+ *         return counter;
+ *     }
+ *
+ *     public static int countAllGreaterElementsInAnArray(int[] initialArray, int point) {
+ *         return initialArray.length - countAllNotGreaterElementsInAnArray(initialArray, point);
+ *     }
+ *
+ *
+ *     private static int partitionArray(int[] arrayToBePartitioned, int startIndex, int endIndex) {
+ *         int pivot = arrayToBePartitioned[endIndex];
+ *
+ *         int i = startIndex - 1;
+ *
+ *         for (int j = startIndex; j < endIndex; j++) {
+ *             if (arrayToBePartitioned[j] <= pivot) {
+ *                 i++;
+ *                 int temp = arrayToBePartitioned[i];
+ *                 arrayToBePartitioned[i] = arrayToBePartitioned[j];
+ *                 arrayToBePartitioned[j] = temp;
+ *             }
+ *         }
+ *
+ *         int temp = arrayToBePartitioned[i + 1];
+ *         arrayToBePartitioned[i + 1] = arrayToBePartitioned[endIndex];
+ *         arrayToBePartitioned[endIndex] = temp;
+ *
+ *         return i + 1;
+ *     }
+ *
+ *
  * }
  */
