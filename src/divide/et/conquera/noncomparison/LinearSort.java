e94b94df8a0496db683d3f9edfeeed15ed0268bc
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-08-31 23:24:34 +0300

    first submission

--- src/divide/et/conquera/noncomparison/LinearSort.java
+++ src/divide/et/conquera/noncomparison/LinearSort.java
@@ -15,10 +15,41 @@ class LinearSort {
             for (int i = 0; i < stringNumbers.length; i++) {
                 numbers[i] = Integer.parseInt(stringNumbers[i]);
             }
-            System.out.println("2 2 3 9 9");
+            int[] sortedNumbers = linearSort(numbers);
+            String[] strings = new String[stringNumbers.length];
+            for (int i = 0; i < stringNumbers.length; i++) {
+                strings[i] = String.valueOf(sortedNumbers[i]);
+            }
+            System.out.println(String.join(" ", strings));
+
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static int[] linearSort(int[] numbers) {
+        int high = 10;
+        int[] numberFrequencies = new int[high];
+        int[] additionalArray = new int[numbers.length];
+        for (int currentNumber : numbers) {
+            // j is a counter fo the initial array
+            numberFrequencies[currentNumber - 1] = numberFrequencies[currentNumber - 1] + 1;
+        }
+        int k = 0;
+        while (k < numberFrequencies.length - 1) {
+            // actual number is k + 1
+            int j = 0;
+            while (j < additionalArray.length) {
+                int numberCount = numberFrequencies[k];
+                while (numberCount > 0) {
+                    numberCount--;
+                    additionalArray[j] = k + 1;
+                    j++;
+                }
+                k++;
+            }
+        }
+        return additionalArray;
     }
 }
