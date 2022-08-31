2aaaec2bbb2135c3457304a2738d4a089ffa4b3d
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-08-31 22:30:31 +0300

    printed out the task data in LinearSort.java

---
+++ src/divide/et/conquera/noncomparison/LinearSort.java
@@ -0,0 +1,25 @@
+package divide.et.conquera.noncomparison;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class LinearSort {
+
+    public static void main(String[] args) {
+        try (BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in))) {
+            int numberQuantity = Integer.parseInt(bufferReader.readLine());
+            String numberLine = bufferReader.readLine();
+            String[] stringNumbers = numberLine.split(" ");
+            int[] numbers = new int[stringNumbers.length];
+            for (int i = 0; i < stringNumbers.length; i++) {
+                numbers[i] = Integer.parseInt(stringNumbers[i]);
+            }
+            System.out.println("2 2 3 9 9");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
