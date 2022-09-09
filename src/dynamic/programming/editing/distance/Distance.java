5fc1e17d84499cd6458f825ee413d34d297f817d
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-09-09 16:50:25 +0300

    filling the matrix

---
+++ src/dynamic/programming/editing/distance/Distance.java
@@ -0,0 +1,88 @@
+package dynamic.programming.editing.distance;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+class Distance {
+    public static void main(String[] args) {
+        try (BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in))) {
+            String firstLine = bufferReader.readLine();
+            String secondLine = bufferReader.readLine();
+            char[] firstWordSymbols = firstLine.toCharArray();
+            char[] secondWordSymbols = secondLine.toCharArray();
+            calculateEditingDistance(firstWordSymbols, secondWordSymbols);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int calculateEditingDistance(char[] firstWordSymbols, char[] secondWordSymbols) {
+        int firstLength = firstWordSymbols.length;
+        int secondLength = secondWordSymbols.length;
+
+        int[][] twoDimensionalArray = new int[firstLength + 1][secondLength + 1];
+        for (int i = 0; i <= firstLength; i++) {
+            for (int j = 0; j <= secondLength; j++) {
+                twoDimensionalArray[i][j] = -1;
+            }
+        }
+
+        // TODO initialize the first column and the first line
+
+        for (int i = 0; i <= firstLength; i++) {
+            if (i == 0 || i == 1) {
+                twoDimensionalArray[i][0] = 0;
+            } else {
+                twoDimensionalArray[i][0] = i - 1;
+            }
+        }
+
+
+        for (int i = 0; i <= secondLength; i++) {
+            if (i == 0 || i == 1) {
+                twoDimensionalArray[0][i] = 0;
+            } else {
+                twoDimensionalArray[0][i] = i - 1;
+            }
+        }
+
+        for (int i = 1; i <= firstLength; i++) {
+            for (int j = 1; j <= secondLength; j++) {
+                if(i == 1 && j == 1){
+                    twoDimensionalArray[i][j] = 0;
+                }
+                //TODO берем только такие квадратики, где одна минус единица.
+
+                int diff = findDiff(firstWordSymbols[i - 1], secondWordSymbols[j - 1]);
+
+
+                // twoDimensionalArray[i][j] = findMinimum(twoDimensionalArray[i-2][j-2]);
+            }
+        }
+
+
+        printOutTheResultingArray(twoDimensionalArray);
+
+        return 0;
+    }
+
+    private static int findDiff(char firstWordSymbol, char secondWordSymbol) {
+        if (firstWordSymbol == secondWordSymbol) {
+            return 0;
+        }
+        return 1;
+    }
+
+
+    private static void printOutTheResultingArray(int[][] twoDimensionalArray) {
+        for (int i = 0; i < twoDimensionalArray.length; i++) {
+            for (int j = 0; j < twoDimensionalArray[0].length; j++) {
+                System.out.printf("%5d ", twoDimensionalArray[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
